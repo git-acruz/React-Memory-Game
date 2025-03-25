a90ea7814e89368b47452c5ec8cdaa08530f292d
Arelvee <alvinc.cruz07@gmail.com>
2025-03-25 20:11:27 +0800

    Updates

--- memory-game/src/components/Options.js
+++
@@ -1,9 +0,0 @@
-
-export default function Options({ valArray }) {
-    const optionsEl = valArray.map(({ name, value }) => (
-        <option key={value} value={value}>
-            {name ? name : value}
-        </option>
-    ))
-    return <>{optionsEl}</>
-}
